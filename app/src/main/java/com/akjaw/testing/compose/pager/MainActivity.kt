a6e62d171666e4ff1d077339dca7881c239baca7
AKJAW <akjaworski1@gmail.com>
2022-02-06 15:00:05 +0100

    initial commit

---
+++ app/src/main/java/com/akjaw/testing/compose/pager/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.akjaw.testing.compose.pager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.akjaw.testing.compose.pager.ui.theme.TestingComposePagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestingComposePagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TestingComposePagerTheme {
+        Greeting("Android")
+    }
+}
