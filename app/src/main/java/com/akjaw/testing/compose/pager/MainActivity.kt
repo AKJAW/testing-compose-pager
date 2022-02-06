808ccffaa781b36bebfea4d24f331b8fbd751b25
AKJAW <akjaworski1@gmail.com>
2022-02-06 15:25:19 +0100

    feat: add scaffold for pager screens

--- app/src/main/java/com/akjaw/testing/compose/pager/MainActivity.kt
+++ app/src/main/java/com/akjaw/testing/compose/pager/MainActivity.kt
@@ -3,11 +3,18 @@ package com.akjaw.testing.compose.pager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.rounded.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -48,9 +55,47 @@ fun MainScreen() {
                         onStaticClicked = { destination = Destination.StaticPager },
                     )
                 }
-                Destination.DynamicPager -> { Text(text = "Dynamic") }
-                Destination.StaticPager -> { Text(text = "Static") }
+                Destination.DynamicPager -> {
+                    PagerScaffold(
+                        title = "Dynamic",
+                        onClose = { destination = Destination.Home }
+                    ) {
+                        Text(text = "Dynamic")
+                    }
+                }
+                Destination.StaticPager -> {
+                    PagerScaffold(
+                        title = "Static",
+                        onClose = { destination = Destination.Home }
+                    ) {
+                        Text(text = "Static")
+                    }
+                }
             }
         }
+    }
+}
+
+@Composable
+fun PagerScaffold(
+    title: String,
+    onClose: () -> Unit,
+    content: @Composable () -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "close",
+                        modifier = Modifier.clickable { onClose() }
+                    )
+                },
+                title = { Text(text = title) }
+            )
+        }
+    ) {
+        content()
     }
 }
